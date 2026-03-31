5f210b3d1cdd92ff339e7107a0e2025e57f39afa
Jv01jooao <jvjooao01@gmail.com>
2026-03-31 18:25:50 -0300

    .
    Apenas Home e Search funciona

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Netflix.MVVM;
+
+namespace Netflix
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void moviserie_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Lógica desejada ao carregar cada moviserie (sender é o UserControl moviserie)
+        }
+
+        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Searchfundo.Visibility = Visibility.Visible;
+        }
+
+        private void Home_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Homefundo.Visibility = Visibility.Visible;
+        }
+
+        private void TextBlock_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Showfundo.Visibility = Visibility.Visible;
+        }
+
+        private void TextBlock_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            Moviesfundo.Visibility = Visibility.Visible;
+        }
+
+        private void TextBlock_MouseEnter_3(object sender, MouseEventArgs e)
+        {
+            MyNetflixfundo.Visibility = Visibility.Visible;
+        }
+
+        private void Search_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Searchfundo.Visibility = Visibility.Collapsed;
+        }
+
+        private void Home_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Homefundo.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBlock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Showfundo.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBlock_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Moviesfundo.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBlock_MouseLeave_2(object sender, MouseEventArgs e)
+        {
+            MyNetflixfundo.Visibility = Visibility.Collapsed;
+        }
+
+        private void Home_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            GridSearch.Visibility = Visibility.Collapsed;
+        }
+
+        private void Search_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            GridSearch.Visibility = Visibility.Visible;
+        }
+
+        public void verifySearch()
+        {
+            if (SearchTextbox.Text != string.Empty)
+            {
+                quadradSearch.Visibility = Visibility.Visible;
+            }else if(SearchTextbox.Text == String.Empty)
+            {
+                quadradSearch.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            verifySearch();
+        }
+    }
+}
---
+++ View/moviserie/moviserie.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Netflix.View.moviserie
+{
+    /// <summary>
+    /// Interação lógica para moviserie.xam
+    /// </summary>
+    public partial class moviserie : UserControl
+    {
+        public moviserie()
+        {
+            InitializeComponent();
+        }
+
+        private void quadrado_MouseLeave(object sender, MouseEventArgs e)
+        {
+            quadrado.Width = 100;
+            quadrado.Margin = new Thickness(0, 0, 0, 0);
+        }
+
+        private void quadrado_MouseEnter(object sender, MouseEventArgs e)
+        {
+            quadrado.Width = 250;
+            
+        }
+    }
+}
